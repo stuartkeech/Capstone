d556e813b20cb4fd8faf60ee28284046484a3337
stuartkeech <stuartkeech@hotmail.ca>
2018-01-25 14:31:08 -0500

    dded stuff

--- ProximityHandler.java
+++ ProximityHandler.java
@@ -1,25 +1,31 @@
+package test;
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiGpioProvider;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.impl.GpioControllerImpl;
+import com.pi4j.io.gpio.impl.GpioPinImpl;
 
 //GPIO17 for trigger, GPIO10 for echo
 public class ProximityHandler {
 	private final int PULSE = 10000;  			// ns = 10us
 	private final int SPEED_OF_SOUND = 34029;	// cm/s
 	
-	private GpioController gpio = new GpioController();
-	private GpioPinDigitalInput echo = new GpioPinDigitalInput();
-	private GpioPinDigitalOutput trigger = new GpioPinDigitalOutput();
+	private GpioControllerImpl gpio = new GpioControllerImpl();
+	private RaspiGpioProvider prov=new RaspiGpioProvider();
+	private GpioPinImpl echo = new GpioPinImpl(gpio,prov,RaspiPin.GPIO_10);
+	private GpioPinImpl trigger = new GpioPinImpl(gpio,prov,RaspiPin.GPIO_17);
 	
 	public ProximityHandler() {
-		gpio = GpioFactory.getInstance();
+		//gpio = GpioFactory.getInstance();
 		
-        trigger = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "PinTrigger", PinState.LOW);
-        echo = gpio.provisionDigitalInputPin(RaspiPin.GPIO_10, "PinEcho", PinPullResistance.PULL_DOWN);
+        //trigger = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "PinTrigger", PinState.LOW);
+        //echo = gpio.provisionDigitalInputPin(RaspiPin.GPIO_10, "PinEcho", PinPullResistance.PULL_DOWN);
         //It really doesn't make sense to me that this is pull up
         
 /*		trigger = (GPIOPin) DeviceManager.open(new GPIOPinConfig(0, triggerPin, 
@@ -30,7 +36,12 @@ public class ProximityHandler {
 				GPIOPinConfig.DIR_INPUT_ONLY, GPIOPinConfig.MODE_INPUT_PULL_UP, 
 				GPIOPinConfig.TRIGGER_NONE, false));*/
 	}
-	
+	public static void main(String[] args){
+		ProximityHandler handle=new ProximityHandler();
+		while(true){
+			System.out.println(handle.getDistance());
+		}
+	}
 	public void destruct() {
 		gpio.shutdown();
 	}
@@ -43,7 +54,12 @@ public class ProximityHandler {
 		
 		//Output 0 to trigger pin, for recommended time between samples
 		trigger.setState(PinState.LOW);
-		Thread.sleep(50);
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		//Set trigger pin to 1
 		trigger.setState(PinState.HIGH);
