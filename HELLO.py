838f3cff8690cda32780164fc2034a34f6e0ae3e
stuartkeech <stuartkeech@hotmail.ca>
2018-04-14 13:38:35 -0400

    added LCD stuff

--- HELLO.py
+++ HELLO.py
@@ -2,5 +2,9 @@ import I2C_LCD_driver
 from time import *
 
 mylcd = I2C_LCD_driver.lcd()
-
+mylcd.backlight(0)
+mylcd.backlight(1)
 mylcd.lcd_display_string("Hello World!", 1,1)
+while(True):
+    mylcd.backlight(0)
+    mylcd.backlight(1)
