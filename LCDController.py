780732e672ff95fc5f43d2a7930f0257225236cc
teamgoodboy <keechsg@mcmaster.ca>
2018-04-22 23:29:55 +0000

    LCD Controller, working code

--- LCDController.py
+++ LCDController.py
@@ -2,37 +2,42 @@ import I2C_LCD_driver
 from time import *
 
 mylcd = I2C_LCD_driver.lcd()
+old_msg = ""
 
 while True:
-    flag=True
+    flag= True
     msg = ""
     while flag:
-        S = open("mj2p.txt","r")
-        msg = S.read()
+        S = open("j2p.txt","r")
+        msg = S.readline()
         S.close()
-        flag = msg==""
+        flag = False
     
-    if msg=="0":
-        mylcd.lcd_display_string("Comms Lost")
-    elif msg=="1":
-        mylcd.lcd_display_string("Weight Limit")
-    elif msg=="2":
-        mylcd.lcd_display_string("ESTOP")
-    elif msg=="3":
-        mylcd.lcd_display_string("Ultra Failed")
-    elif msg=="4":
-        mylcd.lcd_display_string("Infra Failed")
-    elif msg=="5":
-        mylcd.lcd_display_string("Camera Failed")
-    elif msg=="6":
-        mylcd.lcd_display_string("EndE Failed")
-    elif msg=="7":
-        mylcd.lcd_display_string("Drive Failed")
-    elif msg=="8":
-        mylcd.lcd_display_string("Paused")
-    elif msg=="9":
-        mylcd.lcd_display_string("Fetching")
-    elif msg=="10":
-        mylcd.lcd_display_string("Obstacle")
-    else:
-        mylcd.lcd_display_string("Invalid msg code")
+    msg = msg.rstrip("\n")
+    if(old_msg != msg):
+   	 mylcd.lcd_clear()
+   	 if msg=="0":
+       		mylcd.lcd_display_string("Comms Lost")
+    	 elif msg=="1":
+        	mylcd.lcd_display_string("Weight Limit")
+    	 elif msg=="2":
+        	mylcd.lcd_display_string("ESTOP")
+   	 elif msg=="3":
+        	mylcd.lcd_display_string("Ultra Failed")
+    	 elif msg=="4":
+        	mylcd.lcd_display_string("Infra Failed")
+    	 elif msg=="5":
+        	mylcd.lcd_display_string("Camera Failed")
+    	 elif msg=="6":
+        	mylcd.lcd_display_string("EndE Failed")
+    	 elif msg=="7":
+        	mylcd.lcd_display_string("Drive Failed")
+    	 elif msg=="8":
+        	mylcd.lcd_display_string("Paused")
+    	 elif msg=="9":
+        	mylcd.lcd_display_string("Fetching")
+    	 elif msg=="10":
+        	mylcd.lcd_display_string("Obstacle")
+     	 else:
+        	mylcd.lcd_display_string("Invalid msg code")
+    old_msg = msg
